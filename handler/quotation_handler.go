4e8df99d957367ca465657cbfb6dcbd0b23896fe
ivhu <ivhu@foxmail.com>
2024-08-29 17:31:42 +0800

    chore:骂人的一定会通过

--- handler/quotation_handler.go
+++ handler/quotation_handler.go
@@ -115,6 +115,9 @@ func (y *quotationsHandler) CheckUpdate(b *gotgbot.Bot, ctx *ext.Context) bool {
 	for _, i := range 骂 {
 		if strings.Contains(msg, i) {
 			crossR = true
+			if msg == i {
+				return true
+			}
 		}
 	}
 	for _, i := range 神经病 {
