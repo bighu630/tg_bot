d4add2a202297be679bf840757ffd8999d530912
ivhu <ivhu>
2024-11-14 15:27:11 +0800

    bugfix:cannot open kfc db

--- storage/models/quotations.go
+++ storage/models/quotations.go
@@ -2,7 +2,7 @@ package models
 
 type Quotation struct {
 	ID    int    `gorm:"column:id;primaryKey;AUTO_INCREMENT"`
-	Data  string `gorm:"column:data;type:text;not null"`
+	Data  string `gorm:"column:data;type:text"`
 	Level string `gorm:"column:level;type:varchar(255)"`
 }
 
