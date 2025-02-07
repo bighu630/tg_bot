8a18325ae84d5b850ed84560adbee9baaa32a298
ivhu <ivhu@foxmail.com>
2025-02-07 12:26:58 +0800

    feat:切分摘星功能

--- server/server.go
+++ server/server.go
@@ -8,7 +8,6 @@ import (
 	"chatbot/storage"
 	"chatbot/timekeeping"
 	handler "chatbot/webHookHandler"
-	"chatbot/webHookHandler/quotation"
 
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
 )
@@ -19,38 +18,15 @@ func Start() {
 	tgWebHook := connect.NewWebHookConnect(config.GlobalConfig.WebHookConfig)
 	tencent.NewTencentClient(config.GlobalConfig.TencentConfig)
 
-	var ymbHandler ext.Handler
 	var gaiHandler ext.Handler
-	var quotationsHandler ext.Handler
-	if config.GlobalConfig.Ytdlp.Enable {
-		ymbHandler = handler.NewYoutubeHandler(config.GlobalConfig.Ytdlp.Path)
-		tgWebHook.RegisterHandler(ymbHandler)
-	}
 	if config.GlobalConfig.Ai.Enable {
 		gaiHandler = handler.NewGeminiHandler(config.GlobalConfig.Ai)
 		tgWebHook.RegisterHandler(gaiHandler)
 	}
-	if config.GlobalConfig.Storage.Enable {
-		quotationsHandler = handler.NewQuotationsHandler()
-		tgWebHook.RegisterHandler(quotationsHandler)
-	}
-	quotationCtrl, err := quotation.NewQuotationHandler()
-	if err != nil {
-		print("无法初始化 --语录控制器-- ", err)
-	}
-	quotationCtrl.Register(tgWebHook.RegisterHandlerWithCmd)
-	tgWebHook.RegisterHandler(quotationCtrl)
-	tgWebHook.RegisterHandler(quotationCtrl.NewCallbackHander())
-
-	// audioHandler := handler.NewAudioHandler()
-	// tgWebHook.RegisterHandler(audioHandler)
 	timer := timekeeping.NewTimekeeper()
 
 	timer.RegisterCmd(tgWebHook.RegisterHandlerWithCmd)
 	timer.Start()
-
-	// help帮助
-	tgWebHook.RegisterHandlerWithCmd(handler.NewHelpHandler(), "help")
 
 	// tgAutoCall.Start()
 	tgWebHook.Start()
--- webHookHandler/quotation/keyWord.go
+++
@@ -1,4 +0,0 @@
-package quotation
-
-type keyWord struct {
-}
