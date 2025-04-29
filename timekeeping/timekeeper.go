9073c7cd2603951cf808c67b3480c834ad8985c0
ivhu <ivhu@foxmail.com>
2025-04-29 20:47:44 +0800

    chore:source

--- timekeeping/timekeeper.go
+++
@@ -1,8 +0,0 @@
-package timekeeping
-
-import "github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
-
-type Timer interface {
-	Start()
-	Register(func(handler handlers.Response, cmd string))
-}
--- webHookHandler/bot_ctrl/control.go
+++
@@ -1,4 +0,0 @@
-package botctrl
-
-type BotCtrl struct {
-}
